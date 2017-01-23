0ba35da168462cc06b895186825c2f43e9399445
Henry Suryawirawan <henry.ken@gmail.com>
2017-01-23 13:54:43 +0800

    Updated the getOnBoardingMessage to construct the message from 2 files on Google Drive

--- src/new-joiner/index.js
+++ src/new-joiner/index.js
@@ -1,12 +1,71 @@
-'use strict'
+'use strict';
+
+const async = require('async')
+
+const google = require('googleapis');
+const drive = google.drive('v3');
 
 function getWelcomeMessage(newMember) {
   return `Welcome ${newMember.first_name}! (@${newMember.username})\n\n${getOnBoardingMessage()}`
 }
 
 function getOnBoardingMessage() {
-  const ONBOARDING_DOC = 'https://thoughtworks.jiveon.com/groups/people-space-singapore/blog/2016/12/19/welcome-to-thoughtworks-singapore'
-  return `Here are some links you may find helpful:\n<a href="${ONBOARDING_DOC}">TWSG Onboarding Info Pack</a>`
+  return new Promise(
+    function (resolve, reject) {
+      async.parallel([
+        readOnboardingMessageFile,
+        readTelegramGroupsFile
+      ],
+      function (err, results) {
+        if (err) {
+          reject(err)
+        }
+
+        const onboardingMessage = results[0]
+        const telegramGroups = results[1]
+
+        resolve(`${onboardingMessage}\n\n${telegramGroups}`)
+      })
+    }
+  )
+}
+
+function readOnboardingMessageFile(callback) {
+  readFileFromDrive(process.env.ONBOARDING_MESSAGE_FILE_ID, callback);
+}
+
+function readTelegramGroupsFile(callback) {
+  readFileFromDrive(process.env.TELEGRAM_GROUPS_FILE_ID, callback);
+}
+
+function readFileFromDrive(fileId, callback) {
+  const jwtClient = createGoogleClient();
+
+  jwtClient.authorize(function (err) {
+    if (err) {
+      callback(err, null)
+    }
+
+    drive.files.get({
+      fileId: fileId,
+      auth: jwtClient,
+      alt: 'media'
+    }, function (err, resp) {
+      callback(null, resp)
+    })
+  })
+}
+
+function createGoogleClient() {
+  const jsonKey = JSON.parse(process.env.JSON_KEY)
+
+  return new google.auth.JWT(
+    jsonKey.client_email,
+    null,
+    jsonKey.private_key,
+    ['https://www.googleapis.com/auth/drive.readonly'],
+    null
+  )
 }
 
 module.exports = {
