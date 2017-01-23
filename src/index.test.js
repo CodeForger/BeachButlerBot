2584a67f6814d3e859c7101207ee69a2686b0619
Chenshu <ycswaves@gmail.com>
2017-01-23 12:03:27 +0800

    add test for index

---
+++ src/index.test.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect
+const Sinon = require('sinon')
+
+const HandleBotUpdate = require('../index').handleBotUpdate
+const Helper = require('./helper')
+const NewJoiner = require('./new-joiner')
+
+const NewJoinerFixture = require('../fixtures/newJoiner.json')
+const CommandFixture = require('../fixtures/command.json')
+
+
+describe("HandleBotUpdate", function() {
+  const context = ''
+  const callback = Sinon.spy()
+
+  it("should call NewJoiner when there's new joiner", function() {
+    const welcome = Sinon.spy(NewJoiner, 'getWelcomeMessage')
+    HandleBotUpdate(NewJoinerFixture, context, callback)
+    expect(welcome.called).to.be.true
+  })
+
+  it("should call command handler when there's a command", function() {
+    const extractCommand = Sinon.spy(Helper, 'extractCommand')
+    HandleBotUpdate(CommandFixture, context, callback)
+    expect(extractCommand.called).to.be.true
+  })
+})
